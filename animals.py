4f2f00200a0641bdad083fbc93a26ed240d9f7e7
PhiferTristan <phifer.tristan@outlook.com>
2023-10-09 10:34:54 -0500

    chapter 2 refactored classes init to accept two arguments: name and species

--- animals.py
+++ animals.py
@@ -3,159 +3,144 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Goat:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Sheep:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Alpaca:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Horse:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Python:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.slithering = True
         self.date_added = date.today()
 
 class Boa:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.slithering = True
         self.date_added = date.today()
 
 class Viper:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.slithering = True
         self.date_added = date.today()
 
 class Sunbeam:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.slithering = True
         self.date_added = date.today()
 
 class Splitjaw:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.slithering = True
         self.date_added = date.today()
 
 class Turtle:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Newt:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Koi:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Catfish:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Toad:
-    def __init__(self) -> None:
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species) -> None:
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 
 
-miss_fuzz = Llama()
-miss_fuzz.name = "Miss Fuzz"
-miss_fuzz.species = "domestic llama"
-peaches = Goat()
-peaches.name = "Peaches"
-peaches.species = "domestic goat"
-patches = Sheep()
-patches.name = "Goat"
-patches.species = "domestic sheep"
-alfonz = Alpaca()
-alfonz.name = "Alfonz"
-alfonz.species = "domestic alpaca"
-kaptain = Horse()
-kaptain.name = "Kaptain"
-kaptain.species = "Mustang"
-pybert = Python()
-pybert.name = "Pybert"
-pybert.species = "Ball Python"
-bob = Boa()
-bob.name = "Bob"
-bob.species = "Long Tail Boa"
-val = Viper()
-val.name = "Val"
-val.species = "Cottonmouth"
-shorty = Sunbeam()
-shorty.name = "Shorty"
-shorty.species = "Shining sunbeam"
-viktor = Splitjaw()
-viktor.name = "Viktor"
-viktor.species = "smooth-scaled splitjaw"
-john = Turtle()
-john.name = "John"
-john.species = "Painted turtle"
-neal = Newt()
-neal.name = "Neal"
-neal.species = "Eastern newt"
-kyle = Koi()
-kyle.name = "Kyle"
-kyle.species = "Bekko"
-kat = Catfish()
-kat.name = "Kat"
-kat.species = "Blue catfish"
-jiraya = Toad()
-jiraya.name = "Jiraya"
-jiraya.species = "scroll toad"
+miss_fuzz = Llama("Miss Fuzz", "domestic llama")
+
+peaches = Goat("Peaches", "domestic goat")
+
+patches = Sheep("Goat", "domestic sheep")
+
+alfonz = Alpaca("Alfonz", "domestic alpaca")
+
+kaptain = Horse("Kaptain", "Mustang")
+
+pybert = Python("Pybert", "Ball Python")
+
+bob = Boa("Bob", "Long Tail Boa")
+
+val = Viper("Val", "Cottonmouth")
+
+shorty = Sunbeam("Shorty", "Shining sunbeam")
+
+viktor = Splitjaw("Viktor", "smooth-scaled splitjaw")
+
+john = Turtle("John", "Painted turtle")
+
+neal = Newt("Neal", "Eastern newt")
+
+kyle = Koi("Kyle", "Bekko")
+
+kat = Catfish("Kat", "Blue catfish")
+
+jiraya = Toad("Jiraya", "scroll toad")
+
 
 print(miss_fuzz.date_added)
 print(jiraya.name)
